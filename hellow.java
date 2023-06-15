266a1da795c30de9367692f4b83f2dee738208b3
s1300101 <s1300101@u-aizu.ac.jp>
2023-06-15 19:39:34 +0900

    Greetings added

--- hellow.java
+++ hellow.java
@@ -1,17 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class hellow{
     public static void main(String [] args){
         Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
 
         int number1 = rand.nextInt(6) + 1;
         int number2 = rand.nextInt(6) + 1;
         int sum = number1 + number2;
 
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("hello," + name);
 
         System.out.println("Rolling dice...");
         System.out.println("Die 1:" + number1 );
         System.out.println("Die 2:" + number2);
         System.out.println("total value;" + sum);
+
     }
 }
