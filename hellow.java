8abade1cad391acd01a604c222709ed8b043eff8
s1300101 <s1300101@u-aizu.ac.jp>
2023-06-15 19:32:10 +0900

    Greetings added

--- hellow.java
+++ hellow.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class hellow{
     public static void main(String [] args){
-        System.out.println("こんちは、世界！")
+        Random rand = new Random();
+
+        int number1 = rand.nextInt(6) + 1;
+        int number2 = rand.nextInt(6) + 1;
+        int sum = number1 + number2;
+
+
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1:" + number1 );
+        System.out.println("Die 2:" + number2);
+        System.out.println("total value;" + sum);
     }
 }
